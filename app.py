1c35100a5cee701f1ffd83b0856b21014b704cfa
Ernest <ernest.viol@gmail.com>
2018-01-24 18:13:33 -0800

    Continuous running

--- app.py
+++ app.py
@@ -1,2 +1,22 @@
+import string
+import threading
 import speech_recognition as sr
 
+from threading import Thread
+
+# obtain audio
+def voiceRecognition():
+    while True:
+        audioText = ''
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            audio = r.listen(source)
+            try:
+                audioText = r.recognize_google(audio)
+                print(audioText)
+            except sr.UnknownValueError:
+                pass
+
+
+if __name__ == '__main__':
+    Thread(target = voiceRecognition).start()
