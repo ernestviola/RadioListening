ace27219e3796bdf5e204796d0de06f5b350c1d1
Ernest <ernest.viol@gmail.com>
2018-01-28 10:36:14 -0800

    Added an example config

---
+++ example.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import preprocessing
+configuration = {'numbers': ["Your number",],
+         'keywords': ["your keywords"],
+         'account_sid': 'twilio account sid',
+         'twilio_auth_Token': 'twilio auth token',
+         'twilioNumber': 'twilio number',
+         'stationNumber': 'The station to call',
+         }
+use_anonymous = True
